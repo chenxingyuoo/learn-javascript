ffb0d6fef1204d1cc633890186a35a44d63bb9a1
陈星宇 <2731173104@qq.com>
2020-04-11 11:37:23 +0800

    hello-pm2  update pm2 config

--- node/koa/hello-pm2/pm2.config.js
+++ node/koa/hello-pm2/pm2.config.js
@@ -1,5 +1,3 @@
-const origin = typeof process.argv[2] != 'undefined' ? process.argv[2] : 'origin/dev';
-
 module.exports = {
   apps: [
     {
@@ -31,13 +29,24 @@ module.exports = {
   ],
   deploy: {
     production : {
-      user : 'root',                    // ssh 用户
+      user : 'root',                       // ssh 用户
       host : ['101.200.82.95'],            // ssh 地址
       ssh_options: 'StrictHostKeyChecking=no',
-      ref  : 'origin/master',           // git 远程分支
+      ref  : 'origin/master',              // git 远程分支
       repo : 'git@github.com:chenxingyuoo/learn-javascript.git', // git 地址
       path : '/home/project/hello-pm2',     //服务器文件路径
-      'post-deploy' : 'cd node/koa/hello-pm2 && npm install && pm2 reload ecosystem.config.js --env env_production'
+      'post-deploy' : 'cd node/koa/hello-pm2 && npm install && pm2 startOrRestart pm2.config --env env_production'
+    },
+    dev : {
+      user : 'root',
+      host : ['xxx'],
+      ref  : 'origin/master',
+      repo : 'git@github.com:repo.git',
+      path : '/var/www/development',
+      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env dev',
+      env  : {
+        NODE_ENV: 'dev'
+      }
     }
   }
 }
